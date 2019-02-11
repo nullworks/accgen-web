c073ee79b8303f763c0c7b1c08f6a19cf0e156c0
dogecore <dogecore@rape.lol>
2019-02-11 23:43:45 +0300

    ok nope

--- static/js/index.js
+++ static/js/index.js
@@ -32,8 +32,7 @@ function on_generate_click() {
 	$("#generate_progress").show("slow")
 	
 	$.ajax({
-		url: "https://cors.io/?http://35.228.142.240:6969/account",		
-		cache: false
+		url: "35.228.142.240:6969/account"
 	}).done(function(resp) {
 		on_generated(JSON.parse(resp))
 	})
