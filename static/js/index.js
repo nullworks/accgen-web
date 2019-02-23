28f87d21daeeb5cfb58f11784786a81b1d11124b
LightCat <BenCat07@gmail.com>
2019-02-23 13:14:29 +0100

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -51,7 +51,7 @@ function on_generated(acc_data) {
 	
 	$("#acc_login").html(`Login: <strong>${acc_data.login}</strong>`)
 	$("#acc_pass").html(`Password: <strong>${acc_data.password}</strong>`)
-	$("#acc_email").html(`E-Mail address: <a href="https://inboxkitten.com/inbox/${acc_data.email.split("@")[0]}/list">${acc_data.email}</a>`)
+	$("#acc_email").html(`E-Mail address: <a href="https://inboxkitten.com/inbox/${acc_data.email.split("@")[0]}/list" target="_blank">${acc_data.email}</a>`)
 	
 	$("#generated_data").show("slow")
 	$("#generate_button").show("slow")
