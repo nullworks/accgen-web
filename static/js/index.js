d7230b29a63db85fa78f4dba4cec3b119a6afdd7
DedSec <3626859+Ashesh3@users.noreply.github.com>
2019-03-30 04:02:00 +0530

    recaptcha v3 force

--- static/js/index.js
+++ static/js/index.js
@@ -52,14 +52,17 @@ function on_generated(acc_data) {
 }
 
 function on_captcha_valid(token) {
-	return new Promise(function (resolve, reject) {
+	grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
+		action: 'generate'
+	}).then(res => {
+		return new Promise(function (resolve, reject) {
 		init()
 
 		$("#generate_button").hide()
 		$("#generate_progress").show("slow")
 
 		$.ajax({
-			url: "https://catbot.club:2053/account/" + token
+			url: "https://catbot.club:2053/account/" + token + "/" + res
 		}).done(function (resp) {
 			on_generated(resp)
 		})
@@ -67,6 +70,7 @@ function on_captcha_valid(token) {
 		grecaptcha.reset()
 		resolve()
 	})
+	});
 }
 
 var v3_loaded = false;
@@ -79,11 +83,8 @@ async function generate_pressed() {
 	while (!v3_loaded) {
 		await sleep(50)
 	}
-	grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
-		action: 'generate'
-	}).then(res => {
+
 		grecaptcha.execute()
-	})
 }
 
 function on_v2_load() {
