d4e047f3768ff176b4fddd357f29b5b62274a379
TotallyNotElite <1yourexperiment@protonmail.com>
2019-04-02 18:38:14 +0200

    Actively use recaptcha v3 now
    should reduce amount of captchas shown to legit users by a lot

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,7 @@ function init() {
 	$("#generate_error").hide()
 	$("#generate_progress").hide()
 	$("#generated_data").hide()
+	$("#acc_email").hide()
 }
 
 function on_count_received(resp) {
@@ -28,8 +29,6 @@ function perform_count_check() {
 	}).done(function (resp) {
 		on_count_received(resp)
 	})
-
-	setTimeout(perform_count_check, 10000)
 }
 
 function on_generated(acc_data) {
@@ -40,80 +39,78 @@ function on_generated(acc_data) {
 		$("#generate_error_text").text(acc_data.error)
 		return;
 	}
-	if(!multiple)
-	{
 	$("#acc_login").html(`Login: <strong>${acc_data.login}</strong>`)
 	$("#acc_pass").html(`Password: <strong>${acc_data.password}</strong>`)
-	if (acc_data.email == "not_user_accessible_sorry")
-		$("#acc_email").hide();
-	else {
-		$("#acc_email").show();
-		$("#acc_email").html(`E-Mail address: <a href="https://inboxkitten.com/inbox/${acc_data.email.split("@")[0]}/list" target="_blank">${acc_data.email}</a>`)
-	}
-	}else
-	{
-		$("#acc_login").html(`Logins: <strong>${acc_data.login}</strong>, <strong>${acc_data.login}</strong>`)
-	$("#acc_pass").html(`Password: <strong>${acc_data.password}</strong>, <strong>${acc_data.password}</strong>`)
-	$("#acc_email").hide();
-	
-	}
 	$("#generated_data").show("slow")
 	$("#generate_button").show("slow")
 }
 
 function on_captcha_valid(token) {
-	grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
-		action: 'generate'
-	}).then(res => {
-		return new Promise(function (resolve, reject) {
-		init()
+	init()
 
-		$("#generate_button").hide()
-		$("#generate_progress").show("slow")
+	$("#generate_button").hide()
+	$("#generate_progress").show("slow")
 
-		$.ajax({
-			url: "https://catbot.club:2053/account/" + token + "/" + res
-		}).done(function (resp) {
-			on_generated(resp)
-		})
-
-		grecaptcha.reset()
-		resolve()
+	$.ajax({
+		url: "http://localhost:2053/acc/v2/" + token
+	}).done(function (resp) {
+		on_generated(resp)
 	})
-	});
+
+	grecaptcha.reset()
 }
 
 var v3_loaded = false;
+var v2_loaded = false;
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function generate_multiple_pressed(){
-	multiple=true;
+async function generate_multiple_pressed() {
+	multiple = true;
 	while (!v3_loaded) {
 		await sleep(50)
 	}
 
-		grecaptcha.execute()
+	grecaptcha.execute()
 }
 async function generate_pressed() {
-	multiple=false;
-	while (!v3_loaded) {
+	while (!v3_loaded || !v2_loaded) {
 		await sleep(50)
 	}
+	// run v3
+	grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
+		action: 'generate'
+	}).then(function (res) {
+		init()
+		$("#generate_button").hide()
+		$("#generate_progress").show("slow")
 
-		grecaptcha.execute()
+		$.ajax({
+			url: "http://localhost:2053/acc/v3/" + res
+		}).done(function (resp) {
+			if (!resp.v2)
+				on_generated(resp)
+			else {
+				$("#generate_button").show("slow")
+				$("#generate_progress").hide()
+				// run v2
+				grecaptcha.execute()
+			}
+		})
+	})
 }
 
 function on_v2_load() {
+	v2_loaded = true
 	init()
-	perform_count_check()
+	setInterval(perform_count_check, 10000)
+	perform_count_check();
 }
 
 function on_v3_load() {
+	v3_loaded = true
 	grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
 		action: 'homepage'
-	}).then(res => {
-		v3_loaded = true
 	})
 }
