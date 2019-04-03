eaad78f79963f4347c81f4e3056a42abdd204770
DedSec <3626859+Ashesh3@users.noreply.github.com>
2019-04-03 17:13:13 +0530

    localstorage

--- static/js/index.js
+++ static/js/index.js
@@ -3,124 +3,146 @@
 var multiple = false;
 
 function init() {
-	$("#generate_error").hide()
-	$("#generate_progress").hide()
-	$("#generated_data").hide()
-	$("#acc_email").hide()
-	$("#generated_accs_table_card").hide()
-	
+    $("#generated_accs_table_card").hide()
+    $("#generate_error").hide()
+    $("#generate_progress").hide()
+    $("#generated_data").hide()
+    $("#acc_email").hide()
+    if (localStorage.getItem("genned_account") != null) {
+        $('#history_button').show();
+    }
+
 }
 
 function on_count_received(resp) {
-	$("#account_count").prop("count", (localStorage.getItem("account_count") || 0)).animate({
-		count: parseInt(resp)
-	}, {
-		duration: 4000,
-		easing: 'swing',
-		step: function (now) {
-			$("#account_count").text(Math.ceil(now))
-		}
-	})
+    $("#account_count").prop("count", (localStorage.getItem("account_count") || 0)).animate({
+        count: parseInt(resp)
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function(now) {
+            $("#account_count").text(Math.ceil(now))
+        }
+    })
 
-	localStorage.setItem("account_count", resp)
+    localStorage.setItem("account_count", resp)
+}
+
+function history_pressed() {
+    $('#genned_accs').empty()
+    $("#generate_error").hide()
+    $("#generate_progress").hide()
+    $("#generated_data").hide()
+    $("#acc_email").hide()
+
+    $("#generated_accs_table_card").show();
+    if (localStorage.getItem("genned_account") != null) {
+
+        $.each(JSON.parse(localStorage.getItem("genned_account")), function(i, item) {
+            $('<tr class="table-primary">').html(
+                "<td>" + item.login + "</td><td>" + item.password + "</td>").appendTo('#genned_accs');
+        })
+
+    }
+
 }
 
 function perform_count_check() {
-	$.ajax({
-		url: "https://catbot.club:2053/count"
-	}).done(function (resp) {
-		on_count_received(resp)
-	})
+    $.ajax({
+        url: "https://catbot.club:2053/count"
+    }).done(function(resp) {
+        on_count_received(resp)
+    })
 }
 
 function on_generated(acc_data) {
-	$("#generate_progress").hide()
+    $("#generate_progress").hide()
 
-	if (acc_data.error) {
-		$("#generate_error").show("slow")
-		$("#generate_error_text").text(acc_data.error)
-		return;
-	}
-	if(localStorage.getItem("genned_account") == null)
-	{
-		localStorage.setItem("genned_account",JSON.stringify([]))
+    if (acc_data.error) {
+        $("#generate_error").show("slow")
+        $("#generate_error_text").text(acc_data.error)
+        return;
+    }
+    if (localStorage.getItem("genned_account") == null) {
+        localStorage.setItem("genned_account", JSON.stringify([]))
 
-	}
-	localStorage.setItem("genned_account",JSON.stringify(JSON.parse(localStorage.getItem("genned_account")).concat(acc_data)));
-	
-	$("#acc_login").html(`Login: <strong>${acc_data.login}</strong>`)
-	$("#acc_pass").html(`Password: <strong>${acc_data.password}</strong>`)
-	$("#generated_data").show("slow")
-	$("#generate_button").show("slow")
+    }
+    localStorage.setItem("genned_account", JSON.stringify(JSON.parse(localStorage.getItem("genned_account")).concat(acc_data)));
+
+    $("#acc_login").html(`Login: <strong>${acc_data.login}</strong>`)
+    $("#acc_pass").html(`Password: <strong>${acc_data.password}</strong>`)
+    $("#generated_data").show("slow")
+    $("#generate_button").show("slow")
 }
 
 function on_captcha_valid(token) {
-	init()
+    init()
 
-	$("#generate_button").hide()
-	$("#generate_progress").show("slow")
+    $("#generate_button").hide()
+    $("#generate_progress").show("slow")
 
-	$.ajax({
-		url: "https://catbot.club:2053/acc/v2/" + token
-	}).done(function (resp) {
-		on_generated(resp)
-	})
+    $.ajax({
+        url: "https://catbot.club:2053/acc/v2/" + token
+    }).done(function(resp) {
+        on_generated(resp)
+    })
 
-	grecaptcha.reset()
+    grecaptcha.reset()
 }
 
 var v3_loaded = false;
 var v2_loaded = false;
 
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function generate_multiple_pressed() {
-	multiple = true;
-	while (!v3_loaded) {
-		await sleep(50)
-	}
+    multiple = true;
+    while (!v3_loaded) {
+        await sleep(50)
+    }
 
-	grecaptcha.execute()
+    grecaptcha.execute()
 }
 async function generate_pressed() {
-	while (!v3_loaded || !v2_loaded) {
-		await sleep(50)
-	}
-	// run v3
-	grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
-		action: 'generate'
-	}).then(function (res) {
-		init()
-		$("#generate_button").hide()
-		$("#generate_progress").show("slow")
+    $("#generated_accs_table_card").hide();
+    while (!v3_loaded || !v2_loaded) {
+        await sleep(50)
+    }
+    // run v3
+    grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
+        action: 'generate'
+    }).then(function(res) {
+        init()
+        $("#generate_button").hide()
+        $("#generate_progress").show("slow")
 
-		$.ajax({
-			url: "https://catbot.club:2053/acc/v3/" + res
-		}).done(function (resp) {
-			if (!resp.v2)
-				on_generated(resp)
-			else {
-				$("#generate_button").show("slow")
-				$("#generate_progress").hide()
-				// run v2
-				gtag('event', 'additional verification');
-				grecaptcha.execute()
-			}
-		})
-	})
+        $.ajax({
+            url: "https://catbot.club:2053/acc/v3/" + res
+        }).done(function(resp) {
+            if (!resp.v2)
+                on_generated(resp)
+            else {
+                $("#generate_button").show("slow")
+                $("#generate_progress").hide()
+                // run v2
+                gtag('event', 'additional verification');
+                grecaptcha.execute()
+            }
+        })
+    })
 }
 
 function on_v2_load() {
-	v2_loaded = true
-	init()
-	setInterval(perform_count_check, 10000)
-	perform_count_check();
+    v2_loaded = true
+    init()
+    setInterval(perform_count_check, 10000)
+    perform_count_check();
 }
 
 function on_v3_load() {
-	v3_loaded = true
-	grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
-		action: 'homepage'
-	})
+    v3_loaded = true
+    grecaptcha.execute('6LfG55kUAAAAANVoyH7VqYns6j_ZpxB35phXF0bM', {
+        action: 'homepage'
+    })
 }
