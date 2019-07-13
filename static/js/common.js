f6fe494b941203deb28677abf6a3de2534de9c3e
TotallyNotElite <1yourexperiment@protonmail.com>
2019-07-13 22:52:47 +0200

    i was told to push this

--- static/js/common.js
+++ static/js/common.js
@@ -647,7 +647,7 @@ function load_settings() {
 
 async function save_clicked() {
     gtag('event', 'settings_saved');
-    if ($("#settings_2cap").val() != "") {
+    if ($("#settings_twocap").val() != "") {
         $.ajax({
             url: `https://2captcha.com/res.php?key=${$("#settings_2cap").val()}&action=getbalance&header_acao=1`
         }).done(function (resp) {
