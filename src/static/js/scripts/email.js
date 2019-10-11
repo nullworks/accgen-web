4cf6f0a6518b6576b009a22fbe0f218c88035a59
TotallyNotElite <1yourexperiment@protonmail.com>
2019-10-11 18:03:21 +0200

    fix issues

--- src/static/js/scripts/email.js
+++ src/static/js/scripts/email.js
@@ -2,18 +2,32 @@ global.$ = require("jquery");
 require("bootstrap");
 var DOMPurify = require("dompurify");
 
+function displayerror(errortext) {
+    if (errortext) {
+        $("#generic_error").show("slow");
+        $("#generic_error").text(errortext);
+    } else
+        $("#generic_error").hide("slow");
+}
+
 global.startPolling = function () {
+    $("#submit").hide("slow");
     $.ajax({
         url: '/userapi/pollemails',
         method: 'POST',
         dataType: 'json',
         contentType: 'application/json',
         data: JSON.stringify({
-            username: user,
-            password: pass,
-            check: false
+            username: $("#username").val(),
+            password: $("#password").val(),
         }),
         success: async function (returnData) {
+            if (!$("#polling_email").is(":visible")) {
+                $("#acc_email").html(`Waiting for emails at <strong>${returnData.address}</strong>`);
+                $("#polling_email").show("slow");
+                $("#recovered_email").hide("slow");
+                displayerror(undefined);
+            }
             if (returnData.email != "") {
                 $("#submit").show("slow");
                 document.getElementById("email_content").innerHTML = DOMPurify.sanitize(returnData.email);
@@ -21,15 +35,12 @@ global.startPolling = function () {
                 $("#email_display").css('padding-right', '27rem')
                 $("#polling_email").hide("slow");
             } else {
-                if (!$("#polling_email").is(":visible")) {
-                    $("#acc_email").html(`Waiting for emails at <strong>${returnData.address}</strong>`);
-                    $("#polling_email").show("slow");
-                }
-                setTimeout(startPolling, 5000, user, pass);
+                setTimeout(startPolling, 5000);
             }
         },
         error: function (xhr, status, error) {
             $("#submit").show("slow");
+            $("#polling_email").hide("slow");
             try {
                 var res = JSON.parse(xhr.responseText);
                 if (res.error)
