ceddeb465eb5e70ebf82e9836b178069ac03f9b8
TotallyNotElite <1yourexperiment@protonmail.com>
2019-10-11 17:05:48 +0200

    update email functionality

--- src/static/js/scripts/email.js
+++ src/static/js/scripts/email.js
@@ -2,54 +2,7 @@ global.$ = require("jquery");
 require("bootstrap");
 var DOMPurify = require("dompurify");
 
-global.EmailAccessCheck = function EmailAccessCheck() {
-    $("#recovered_email").hide("slow");
-    displayerror(undefined);
-    $("#submit").hide("slow");
-
-    if ($("#username").val() == "" || $("#password").val() == "") {
-        $("#error_invalid").show();
-        return;
-    }
-    $.ajax({
-        url: '/userapi/pollemails',
-        method: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            username: $("#username").val(),
-            password: $("#password").val(),
-            check: true
-        }),
-        success: async function (returnData) {
-            $("#acc_email").html(`Waiting for emails at <strong>${returnData.email}</strong>`);
-            $("#polling_email").show("slow");
-            startPolling($("#username").val(), $("#password").val());
-        },
-        error: function (xhr, status, error) {
-            $("#submit").show("slow");
-            try {
-                var res = JSON.parse(xhr.responseText);
-                if (res.error)
-                    displayerror(res.error);
-                else
-                    throw new Error("Object does not contain .error!")
-            } catch (error) {
-                displayerror("Unknown error! Please try again later.");
-            }
-        }
-    })
-}
-
-function displayerror(errortext) {
-    if (errortext) {
-        $("#generic_error").show("slow");
-        $("#generic_error").text(errortext);
-    } else
-        $("#generic_error").hide("slow");
-}
-
-function startPolling(user, pass) {
+global.startPolling = function () {
     $.ajax({
         url: '/userapi/pollemails',
         method: 'POST',
@@ -68,6 +21,10 @@ function startPolling(user, pass) {
                 $("#email_display").css('padding-right', '27rem')
                 $("#polling_email").hide("slow");
             } else {
+                if (!$("#polling_email").is(":visible")) {
+                    $("#acc_email").html(`Waiting for emails at <strong>${returnData.address}</strong>`);
+                    $("#polling_email").show("slow");
+                }
                 setTimeout(startPolling, 5000, user, pass);
             }
         },
