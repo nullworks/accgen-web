e7d54c10cc832b536d9137dc21779f526f053bd3
TotallyNotElite <1yourexperiment@protonmail.com>
2020-07-26 10:38:43 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     common: './src/static/js/scripts/common_base.js',
   },
